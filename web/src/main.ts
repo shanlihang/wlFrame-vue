3744129d82831fbd82732ef7445ac83d2c5b496c
shanlihang <slh_ontuo@126.com>
2024-03-14 11:08:58 +0800

    配置src别名并引入antd

--- web/src/main.ts
+++ web/src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
-import './style.css'
+import './assets/style/reset.css'
 import App from './App.vue'
+import Antd from 'ant-design-vue';
 
-createApp(App).mount('#app')
+createApp(App).use(Antd).mount('#app')
---
+++ web/src/views/LayoutPage.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div>
+    <a-tag color="pink">pink</a-tag>
+    <a-tag color="red">red</a-tag>
+    <a-tag color="orange">orange</a-tag>
+    <a-tag color="green">green</a-tag>
+    <a-tag color="cyan">cyan</a-tag>
+    <a-tag color="blue">blue</a-tag>
+    <a-tag color="purple">purple</a-tag>
+  </div>
+</template>
+
+<style scoped>
+</style>
