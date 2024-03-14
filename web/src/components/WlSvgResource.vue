47b216b675e257e5ca054491fae72ab785b691af
shanlihang <slh_ontuo@126.com>
2024-03-14 14:34:23 +0800

    菜单栏设计

---
+++ web/src/components/WlSvgResource.vue
@@ -0,0 +1,25 @@
+<!-- 
+    1.src无默认值，必须传入图片的绝对路径
+    2.width和height默认为80
+ -->
+<script setup lang="ts">
+interface SvgSource{
+    width?:string|number,
+    height?:string|number,
+    src:string
+}
+
+const props = withDefaults(defineProps<SvgSource>(),{
+    width:20,
+    height:20
+})
+</script>
+
+<template>
+    <div class="svgSource">
+        <img :src="props.src" :style="{'width':props.width+'px',height:props.height+'px'}">
+    </div>
+</template>
+
+<style scoped>
+</style>
--- web/src/views/LayoutPage.vue
+++ web/src/views/LayoutPage.vue
@@ -1,22 +1,103 @@
 <script setup lang="ts">
+import { reactive } from 'vue';
+import WlSvgSource from '@/components/WlSvgResource.vue'
+
+interface MenuOptions{
+  openKeys?:Array<string>,
+  selectedKeys?:Array<string>,
+  collapsed?:boolean,
+  isLight?:boolean,
+  style?:Object
+}
+
+interface MenuList{
+  id:number,
+  icon?:string,
+  title:string,
+  children?:Array<MenuList>
+}
+
+const menuOptions = reactive<MenuOptions>({
+  openKeys:['sub1'],
+  selectedKeys:[],
+  collapsed:false,
+  isLight:true,
+  style:{
+    width:'225px',
+    height:'100%'
+  }
+})
+
+const menuList = reactive<Array<MenuList>>([
+  {id:1,icon:'@/assets/svg/vue.svg',title:'菜单1',children:[
+    {id:5,icon:'@/assets/svg/vue.svg',title:'子菜单1'},
+    {id:6,icon:'@/assets/svg/vue.svg',title:'子菜单2'},
+    {id:7,icon:'@/assets/svg/vue.svg',title:'子菜单3'},
+  ]},
+  {id:2,icon:'@/assets/svg/vue.svg',title:'菜单2'},
+  {id:3,icon:'@/assets/svg/vue.svg',title:'菜单3'},
+])
+
+const isCollapsed = () => {
+  menuOptions.collapsed = !menuOptions.collapsed
+  menuOptions.isLight = !menuOptions.isLight
+  if(menuOptions.collapsed){
+    menuOptions.style.width = '70px'
+  }else{
+    menuOptions.style.width = '225px'
+  }
+}
 </script>
 
 <template>
   <div class="layout">
-    <a-tag color="pink">pink</a-tag>
-    <a-tag color="red">red</a-tag>
-    <a-tag color="orange">orange</a-tag>
-    <a-tag color="green">green</a-tag>
-    <a-tag color="cyan">cyan</a-tag>
-    <a-tag color="blue">blue</a-tag>
-    <a-tag color="purple">purple</a-tag>
+    <div class="menu">
+      <a-menu
+      v-model:openKeys="menuOptions.openKeys"
+      v-model:selectedKeys="menuOptions.selectedKeys"
+      mode="inline"
+      :theme="menuOptions.isLight ? 'light' : 'dark'"
+      :inline-collapsed="menuOptions.collapsed"
+      :style="menuOptions.style"
+    >
+    <div class="logo">
+      12312131
+    </div>
+    <a-sub-menu :key="item.id" v-for="item in menuList">
+        <template #icon>
+         <WlSvgSource :src="item.icon!"/>
+        </template>
+        <template #title>{{item.title}}</template>
+        <a-menu-item v-for="i in item.children" :key="i.id">
+          {{i.title}}
+        </a-menu-item>
+      </a-sub-menu>
+  </a-menu>
+    </div>
+    <div class="screen">
+      <button @click="isCollapsed">132</button>
+    </div>
+    
+    
   </div>
 </template>
 
-<style scoped>
+<style scoped lang="less">
 .layout{
     width: 100%;
     height: 100vh;
-    background-color: aqua;
+    display: flex;
+    .menu{
+      .logo{
+        height: 100px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+      }
+    }
+    .screen{
+      flex: 1 0 auto;
+      background-color: brown;
+    }
 }
 </style>
