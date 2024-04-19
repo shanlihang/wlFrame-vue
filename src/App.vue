cfc5ed0301022a5633a081487f9fa620134a44f8
slh_home <slh67490009@gmail.com>
2024-04-19 23:47:11 +0800

    完成菜单组件的编写

--- src/App.vue
+++ src/App.vue
@@ -1,65 +1,47 @@
 <template>
-  <div class="layout page">
-    <div class="menu box">
+  <div class="layout">
+    <div class="menu">
       <MenuList />
     </div>
     <div class="area">
-      <div class="header box">123456</div>
-      <div class="main box">123156456</div>
+      <div class="header">123456</div>
+      <div class="main">123156456</div>
     </div>
   </div>
 </template>
 <script lang="ts" setup>
-import {useSystemStore} from '@/store/system'
 import MenuList from '@/components/menu/MenuList.vue';
-
-const system = useSystemStore()
 
 
 </script>
 <style scoped>
 .layout{
+  height: 100vh;
   display: flex;
+  background-color: #f8f8f8;
 }
 .layout .menu{
   height: 100%;
+  background-color: #fff;
 }
 .layout .area{
   height: 100%;
   flex: 1 0 auto;
   display: flex;
   flex-direction: column;
+  background-color: #eeeeee;
 }
 .layout .area .header{
   margin-left: 1px;
   height: 50px;
-  
+  background-color: #fff;
 }
 .layout .area .main{
   width: 99%;
   flex :1 0 auto;
-  margin: 10px auto 0;
+  margin: 10px auto 10px;
   border-radius: 6px;
   overflow: hidden;
-}
-</style>
-
-<style>
-.page{
-  width: 100%;
-  height: 100vh;
-  background-color: v-bind(system.theme.pageBgColor);
-  color: v-bind(system.theme.mainFontColor);
-}
-.box{
-  background-color: v-bind(system.theme.boxBgColor);
-  color: v-bind(system.theme.mainFontColor);
-}
-.mainFont{
-  color: v-bind(system.theme.mainFontColor);
-}
-
-.subFont{
-  color: v-bind(system.theme.subFontColor);
+  background-color: #fff;
 }
 </style>
--- src/components/menu/MenuList.vue
+++ src/components/menu/MenuList.vue
@@ -1,16 +1,177 @@
 <script setup lang="ts">
 
+const list = [
+  {
+    "ID": 3,
+    "CreatedAt": "2024-04-05T21:33:06+08:00",
+    "UpdatedAt": "2024-04-05T21:33:06+08:00",
+    "DeletedAt": null,
+    "name": "系统",
+    "component": "nulla officia sunt aliqua eu",
+    "icon": "http://dummyimage.com/100x100",
+    "parentId": null,
+    "router": "Excepteur mollit aute aliquip dolor",
+    "sort": 1,
+    "type": 0,
+    "uri": "Duis sint",
+    "children": [
+      {
+        "ID": 4,
+        "CreatedAt": "2024-04-06T18:49:10+08:00",
+        "UpdatedAt": "2024-04-06T18:49:10+08:00",
+        "DeletedAt": null,
+        "name": "用户管理",
+        "component": "veniam cupidatat exercitation",
+        "icon": "http://dummyimage.com/100x100",
+        "parentId": 9,
+        "router": "magna adipisicing velit pariatur",
+        "sort": 1,
+        "type": 1,
+        "uri": "occaecat fugiat nostrud dolore",
+        "children": null
+      },
+      {
+        "ID": 5,
+        "CreatedAt": "2024-04-06T18:49:15+08:00",
+        "UpdatedAt": "2024-04-06T18:49:15+08:00",
+        "DeletedAt": null,
+        "name": "角色管理",
+        "component": "nulla mollit",
+        "icon": "http://dummyimage.com/100x100",
+        "parentId": 9,
+        "router": "esse ipsum",
+        "sort": 1,
+        "type": 1,
+        "uri": "elit proident labore",
+        "children": null
+      }
+    ]
+  },
+  {
+    "ID": 9,
+    "CreatedAt": "0001-01-01T00:00:00Z",
+    "UpdatedAt": "0001-01-01T00:00:00Z",
+    "DeletedAt": null,
+    "name": "超级管理员",
+    "component": null,
+    "icon": null,
+    "parentId": null,
+    "router": null,
+    "sort": null,
+    "type": 0,
+    "uri": null,
+    "children": [
+      {
+        "ID": 4,
+        "CreatedAt": "2024-04-06T18:49:10+08:00",
+        "UpdatedAt": "2024-04-06T18:49:10+08:00",
+        "DeletedAt": null,
+        "name": "用户管理",
+        "component": "veniam cupidatat exercitation",
+        "icon": "http://dummyimage.com/100x100",
+        "parentId": 9,
+        "router": "magna adipisicing velit pariatur",
+        "sort": 1,
+        "type": 1,
+        "uri": "occaecat fugiat nostrud dolore",
+        "children": null
+      },
+      {
+        "ID": 5,
+        "CreatedAt": "2024-04-06T18:49:15+08:00",
+        "UpdatedAt": "2024-04-06T18:49:15+08:00",
+        "DeletedAt": null,
+        "name": "角色管理",
+        "component": "nulla mollit",
+        "icon": "http://dummyimage.com/100x100",
+        "parentId": 9,
+        "router": "esse ipsum",
+        "sort": 1,
+        "type": 1,
+        "uri": "elit proident labore",
+        "children": null
+      },
+      {
+        "ID": 6,
+        "CreatedAt": "2024-04-06T18:49:22+08:00",
+        "UpdatedAt": "2024-04-06T18:49:22+08:00",
+        "DeletedAt": null,
+        "name": "权限管理",
+        "component": "laboris ad",
+        "icon": "http://dummyimage.com/100x100",
+        "parentId": 9,
+        "router": "magna adipisicing velit pariatur",
+        "sort": 1,
+        "type": 1,
+        "uri": "tempor in Ut",
+        "children": null
+      }
+    ]
+  }
+]
+
 </script>
 
 <template>
   <div class="menuList box">
-
+    <div class="menu">
+      <div class="logo">
+        <slot name="logo">LOGO</slot>
+      </div>
+      <div class="list" v-for="item in list" :key="item.ID">
+        <div class="title">{{ item.name }}</div>
+        <div class="child">
+          <div class="sub" v-for="i in item.children" :key="i.ID">
+            <div class="i">{{ i.name }}</div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
-<style scoped>
-.menuList{
-    height: 100vh;
-    width: 256px;
+<style scoped lang="less">
+.menuList {
+  height: 100vh;
+  width: 256px;
+
+  .menu {
+    margin: 0 20px;
+    height: 100%;
+
+    .logo {
+      margin-top: 10px;
+      height: 80px;
+      background-color: aliceblue;
+    }
+
+    .list {
+      .title {
+        font-weight: bold;
+        height: 40px;
+        line-height: 40px;
+        border-bottom: 2px solid #f0f0f0;
+        color: #8c8cbc;
+        margin-bottom: 20px;
+        text-indent: 2em;
+      }
+      .child{
+        text-indent: 4em;
+        font-size: 15px;
+        .sub{
+          margin: 5px 10px;
+          border-radius: 6px;
+          .i{
+            height: 30px;
+            line-height: 30px;
+            cursor: pointer;
+          }
+        }
+        .sub:hover{
+          background-color: rgba(0, 0, 0, .1);
+        }
+      }
+    }
+  }
 }
 </style>
