de6810c2e9aed3d6353f8056b73f46134cbbc7e9
slh_home <slh67490009@gmail.com>
2024-06-10 22:51:36 +0800

    用户新增删除接口的完善

--- src/views/system/UserManage.vue
+++ src/views/system/UserManage.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
-import { reactive,onMounted } from 'vue';
+import { ref,reactive,onMounted } from 'vue';
 import {getUserList,deleteUser,addUser} from '@/api/user'
+import {getRoleList} from '@/api/role'
+import {message,Modal} from 'ant-design-vue'
 
 interface Table{
     key:string,
@@ -13,29 +15,56 @@ interface Table{
 }
 
 interface AddForm{
-    key:string,
     name:string,
-    sex:number,
+    sex:number|undefined,
+    username:string,
     phone:string,
     email:string,
     birthday:string,
     roles:string[],
 }
 
+interface UpdateForm{
+    ID?:number,
+    name:string,
+    sex:number|undefined,
+    username:string,
+    phone:string,
+    email:string,
+    birthday:string,
+    roles:string[],
+    CreatedAt?:string,
+    UpdatedAt?:string,
+    DeletedAt?:string
+}
+
+interface Role{
+    ID:number,
+    name:string,
+    desc:string,
+    CreatedAt:string,
+    UpdatedAt:string,
+    DeletedAt:string
+}
+
 interface Data{
     addModal:boolean,
+    updateModal:boolean,
     searchForm:{
         name:string,
         sex:number|undefined,
         phone:string,
-        email:string
+        email:string,
     },
     table:Table[],
-    addForm:AddForm
+    addForm:AddForm,
+    updateForm:UpdateForm,
+    roleList:Role[],
 }
 
 const data = reactive<Data>({
     addModal:false,
+    updateModal:false,
     searchForm:{
         name:'',
         sex:undefined,
@@ -44,21 +73,39 @@ const data = reactive<Data>({
     },
     table:[],
     addForm:{
-        key:'',
+        username:'',
         name:'',
-        sex:0,
+        sex:undefined,
         phone:'',
         email:'',
         birthday:'',
         roles:[],
-    }
+    },
+    roleList:[],
+    updateForm:{
+        username:'',
+        name:'',
+        sex:undefined,
+        phone:'',
+        email:'',
+        birthday:'',
+        roles:[],
+    },
 })
+
+const temp = ref<UpdateForm>()
 
 const columns = [
     {
         title: '姓名',
         dataIndex: 'name',
         key: 'name',
+        align:'center'
+    },
+    {
+        title: '用户名',
+        dataIndex: 'username',
+        key: 'username',
         align:'center'
     },
     {
@@ -100,12 +147,79 @@ const columns = [
 
 const initData = () => {
     getUserList().then(res => {
-        data.table = res.data
-        
+        data.table = res.data 
     })
 }
 
-const handleBaseOk = () => {}
+const handleEdit = (record:AddForm) => {
+    console.log(record);
+    
+    temp.value = record
+    getRoleList().then(res => {
+        data.roleList = res.data
+    })
+    data.updateModal = true
+}
+
+const resetSearch = () => {
+    data.searchForm.name='',
+    data.searchForm.sex=undefined,
+    data.searchForm.phone='',
+    data.searchForm.email=''
+}
+
+const resetAdd = () => {
+    data.addForm.username='',
+    data.addForm.name='',
+    data.addForm.sex=undefined,
+    data.addForm.phone='',
+    data.addForm.email='',
+    data.addForm.birthday='',
+    data.addForm.roles=[]
+}
+
+const openModal = () => {
+    data.addModal=true
+    getRoleList().then(res => {
+        data.roleList = res.data
+    })
+}
+
+const handleBaseOk = () => {
+    addUser(data.addForm).then(res => {
+        if(res.rowAffect == 1){
+            message.success('新增成功')
+            initData()
+        }
+    })
+    data.addModal = false
+    resetAdd()
+}
+
+const handleUpdateOk = () => {
+    data.updateModal = false
+    console.log(temp.value);
+}
+
+const handleDelete = (id:number) => {
+    Modal.confirm({
+        title: '删除确认',
+        content: '您确定要删除这条记录吗？',
+        okText:'确定',
+        cancelText:'取消',
+        onOk() {
+            deleteUser(id).then(res => {
+                if(res.rowAffect == 1){
+                    message.success('删除成功')
+                    initData()
+                }
+            })
+        },
+        onCancel() {
+            message.info('取消删除操作')
+        },
+    });
+}
 
 onMounted(() => {
     initData()
@@ -169,17 +283,17 @@ onMounted(() => {
     <div class="handle">
         <div class="left">
             <a-button class="btn" type="primary">搜索</a-button>
-            <a-button class="btn">重置搜索</a-button>
+            <a-button class="btn" @click="resetSearch">重置搜索</a-button>
         </div>
         
-        <a-button class="btn" type="primary" @click="data.addModal=true">新增</a-button>
+        <a-button class="btn" type="primary" @click="openModal">新增</a-button>
     </div>
     <div class="table">
         <a-table :columns="columns" :data-source="data.table" size="small">
             <template #bodyCell="{ record,column }">
                 <template v-if="column.key === 'action'">
-                    <a-button type="link">编辑</a-button>
-                    <a-button type="link" danger>删除</a-button>
+                    <a-button type="link" @click="handleEdit(record)">编辑</a-button>
+                    <a-button type="link" danger @click="handleDelete(record.ID)">删除</a-button>
                 </template>
                 <template v-else-if="column.key === 'sex'">
                     <span>
@@ -202,6 +316,7 @@ onMounted(() => {
     </div>
     
     <a-modal v-model:open="data.addModal" title="新增用户信息" okText="确认" cancelText="取消" @ok="handleBaseOk">
+        <a-alert message="系统新增用户的初始密码为123456" type="warning" />
         <a-form
             style="margin-top: 20px;"
             :model="data.addForm"
@@ -218,13 +333,32 @@ onMounted(() => {
                 <a-input v-model:value="data.addForm.name" placeholder="请输入姓名" />
             </a-form-item>
             <a-form-item
+                label="用户名"
+                name="username"
+                :rules="[{ required: true, message: '用户名不能为空' }]"
+                >
+                <a-input v-model:value="data.addForm.username" placeholder="请输入用户名" />
+            </a-form-item>
+            <a-form-item
+                label="角色"
+                name="roles"
+                :rules="[{ required: true, message: '角色不能为空' }]"
+                >
+                <a-select
+                    v-model:value="data.addForm.roles"
+                    placeholder="请选择角色"
+                    mode="multiple"
+                >
+                    <a-select-option v-for="i in data.roleList" :key="i.ID" :value="i.ID">{{ i.name }}</a-select-option>
+                </a-select>
+            </a-form-item>
+            <a-form-item
                 label="性别"
                 name="sex"
                 :rules="[{ required: true, message: '性别不能为空' }]"
                 >
                 <a-select
                     v-model:value="data.addForm.sex"
-                    style="width: 120px"
                     placeholder="请选择性别"
                     >
                     <a-select-option :value="1">男</a-select-option>
@@ -248,10 +382,84 @@ onMounted(() => {
             <!-- <a-form-item
                 label="生日"
                 name="birthday"
-                format="YYYY/MM/DD"
+                format="YYYY-MM-DD"
                 :rules="[{ required: true, message: '请选择你的出生日期' }]"
                 >
-                <a-date-picker v-model:value="data.addForm.birthday" />
+                <a-date-picker v-model:value="data.addForm.birthday" placeholder="请选择出生日期" />
+            </a-form-item> -->
+        </a-form>
+    </a-modal>
+
+    <a-modal v-model:open="data.updateModal" title="修改用户信息" okText="确认" cancelText="取消" @ok="handleUpdateOk">
+        <a-form
+            style="margin-top: 20px;"
+            :model="temp"
+            name="basic"
+            :label-col="{ span: 5 }"
+            :wrapper-col="{ span: 16 }"
+            autocomplete="off"
+        >
+            <a-form-item
+                label="姓名"
+                name="name"
+                :rules="[{ required: true, message: '姓名不能为空' }]"
+                >
+                <a-input v-model:value="temp.name" placeholder="请输入姓名" />
+            </a-form-item>
+            <a-form-item
+                label="用户名"
+                name="username"
+                :rules="[{ required: true, message: '用户名不能为空' }]"
+                >
+                <a-input v-model:value="temp.username" placeholder="请输入用户名" />
+            </a-form-item>
+            <a-form-item
+                label="角色"
+                name="roles"
+                :rules="[{ required: true, message: '角色不能为空' }]"
+                >
+                <a-select
+                    v-model:value="temp.roles"
+                    placeholder="请选择角色"
+                    mode="multiple"
+                >
+                    <a-select-option v-for="i in data.roleList" :key="i.ID" :value="i.ID">{{ i.name }}</a-select-option>
+                </a-select>
+            </a-form-item>
+            <a-form-item
+                label="性别"
+                name="sex"
+                :rules="[{ required: true, message: '性别不能为空' }]"
+                >
+                <a-select
+                    v-model:value="temp.sex"
+                    placeholder="请选择性别"
+                    >
+                    <a-select-option :value="1">男</a-select-option>
+                    <a-select-option :value="0">女</a-select-option>
+                </a-select>
+            </a-form-item>
+            <a-form-item
+                label="电话"
+                name="phone"
+                :rules="[{ required: true, message: '电话不能为空' }]"
+                >
+                <a-input v-model:value="temp.phone" placeholder="请输入电话" />
+            </a-form-item>
+            <a-form-item
+                label="邮箱"
+                name="email"
+                :rules="[{ required: true, message: '邮箱不能为空' }]"
+                >
+                <a-input v-model:value="temp.email" placeholder="请输入邮箱" />
+            </a-form-item>
+            <!-- <a-form-item
+                label="生日"
+                name="birthday"
+                format="YYYY-MM-DD"
+                :rules="[{ required: true, message: '请选择你的出生日期' }]"
+                >
+                <a-date-picker v-model:value="temp.birthday" placeholder="请选择出生日期" />
             </a-form-item> -->
         </a-form>
     </a-modal>
