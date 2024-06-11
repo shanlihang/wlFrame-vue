c4c84e363f749a6e3e384d368293c79ac74bf793
shanlihang <slh_ontuo@126.com>
2024-06-11 10:21:03 +0800

    居民管理接口的封装

--- src/api/people.ts
+++ src/api/people.ts
@@ -3,7 +3,7 @@ import request  from "@/utils/request";
 //新增居民
 export function insertPeople(data:object){
     return request({
-        url:'/api/v1/medical/push/add',
+        url:'/api/v1/medical/people/add',
         method:'post',
         data
     })
@@ -12,7 +12,7 @@ export function insertPeople(data:object){
 //获取居民列表
 export function selectPeople(){
     return request({
-        url:'/api/v1/medical/push/list',
+        url:'/api/v1/medical/people/list',
         method:'get'
     })
 }
@@ -20,33 +20,7 @@ export function selectPeople(){
 //根据id删除居民
 export function deletePeopleById(id:number){
     return request({
-        url:'/api/v1/medical/push/'+id,
-        method:'delete',
-    })
-}
-
-
-//新增反馈
-export function insertFeedback(data:object){
-    return request({
-        url:'/api/v1/medical/push/add',
-        method:'post',
-        data
-    })
-}
-
-//获取反馈列表
-export function selectFeedback(){
-    return request({
-        url:'/api/v1/medical/push/list',
-        method:'get'
-    })
-}
-
-//根据id删除反馈
-export function deleteFeedbackById(id:number){
-    return request({
-        url:'/api/v1/medical/push/'+id,
+        url:'/api/v1/medical/people/'+id,
         method:'delete',
     })
 }
