3e2ccece77b6331bc8eb558c628f89b0b3ff1046
slh_ontuo <slh_ontuo@126.com>
2024-06-13 22:32:41 +0800

    请求携带token

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,9 +1,27 @@
 import axios from 'axios'
+import { Session } from '@/utils/storage'
+
 
 const request = axios.create({
-    baseURL:'http://localhost:9090',
-    timeout:5000
+    baseURL: 'http://localhost:9090',
+    timeout: 5000
 })
+
+// 添加请求拦截器
+request.interceptors.request.use(
+    (config) => {
+        // 在发送请求之前做些什么 token
+        if (Session.get('token')) {
+            config.headers!['Authorization'] = `Bearer ${Session.get('token')}`;
+        }
+        return config;
+    },
+    (error) => {
+        // 对请求错误做些什么
+        return Promise.reject(error);
+    }
+);
+
 
 request.interceptors.response.use(
     (response) => {
